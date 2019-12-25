2b9a36db682ab749054b9de8cbaff35025cf32d3
takuya hakiri <tyapi4@yahoo.co.jp>
2019-12-25 19:02:48 +0900

    [Fix]userのnewアクションの不具合を解消

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
     end
   end
   resources :sessions, only: %w!new create destroy!
-  get 'users', to: 'users#index'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
+  resources :users do
+    collection do
+      post :confirm
+    end
+  end
 end
+#   get 'users', to: 'users#index'
+#   get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+#   get 'users/:id', to: 'users#show', as: 'user'
+#   patch 'users/:id', to: 'users#update'
+#   put 'users/:id', to: 'users#update'
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,12 @@ ActiveRecord::Schema.define(version: 2019_12_25_085327) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
     t.string "password", null: false
   end
+
 end
